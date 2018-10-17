9bd49629a2140b22833c3611564fd8b2f90759ed
andreafranco <andrea07021981@gmail.com>
2018-10-17 18:17:48 +0200

    Created first activity

---
+++ app/src/main/java/com/projects/andreafranco/imagegalleryapp/ImageListActivity.java
@@ -0,0 +1,16 @@
+package com.projects.andreafranco.imagegalleryapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.projects.andreafranco.imagegalleryapp.activities.SingleFragmentActivity;
+
+public class ImageListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+}
